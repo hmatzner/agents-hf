1e0a65856ad4e89e598ca5fe3a3b068720188475
hmatzner <hernoo.m@gmail.com>
2025-04-04 15:26:50 +0300

    [add] tools & tokenizer scripts

---
+++ src/tokenizer.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer
+
+messages = [
+    {"role": "system", "content": "You are an AI assistant with access to various tools."},
+    {"role": "user", "content": "Hi !"},
+    {"role": "assistant", "content": "Hi human, what can help you with ?"},
+]
+tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-1.7B-Instruct")
+rendered_prompt = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True)
