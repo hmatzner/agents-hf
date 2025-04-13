ba0756e466814474c82091640d0be1413702ae0d
hmatzner <hernoo.m@gmail.com>
2025-04-14 00:08:37 +0300

    [add] api script

--- src/serverless_api.py
+++ src/serverless_api.py
@@ -1,4 +1,14 @@
 from utils.secrets import get_env_pass
+from huggingface_hub import InferenceClient
 
 hf_token = get_env_pass("HF_TOKEN")
-print(hf_token)
+client = InferenceClient("meta-llama/Llama-3.2-3B-Instruct")
+
+output = client.chat.completions.create(
+    messages=[
+        {"role": "user", "content": "The capital of France is"},
+    ],
+    stream=False,
+    max_tokens=1024,
+)
+print(output.choices[0].message.content)
