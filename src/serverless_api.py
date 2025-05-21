1fc5255ae775045e30b07d072c42bd47cc673b98
hmatzner <hernoo.m@gmail.com>
2025-05-22 00:01:08 +0300

    small fix

--- src/serverless_api.py
+++ src/serverless_api.py
@@ -5,6 +5,7 @@ hf_token = get_env_pass("HF_TOKEN")
 client = InferenceClient("meta-llama/Llama-3.2-3B-Instruct")
 
 output = client.chat.completions.create(
+    
     messages=[
         {"role": "user", "content": "The capital of France is"},
     ],
